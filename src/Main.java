635e190ca236b28a0845078419fced1f2c46ff74
oneyan1 <yangrodno@gmail.com>
2019-11-14 15:44:55 +0100

    create new project files

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.nio.charset.Charset;
+
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.gui.*;
+import com.googlecode.lanterna.gui.Component.Alignment;
+import com.googlecode.lanterna.gui.component.Button;
+import com.googlecode.lanterna.gui.component.EmptySpace;
+import com.googlecode.lanterna.gui.component.Label;
+import com.googlecode.lanterna.gui.component.Panel;
+import com.googlecode.lanterna.gui.component.Panel.Orientation;
+import com.googlecode.lanterna.gui.component.Table;
+import com.googlecode.lanterna.gui.layout.LinearLayout;
+import com.googlecode.lanterna.gui.layout.VerticalLayout;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.ScreenCharacterStyle;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.TerminalSize;
+
+public class Main {
+    public static void main(String[] args) {
+    
+    }
+
+}
