b4f5be9730d2158923d6f9932b3ceeff14d69546
oneyan1 <yangrodno@gmail.com>
2019-11-18 23:30:35 +0100

    fully logical part

--- src/Main.java
+++ src/Main.java
@@ -26,15 +26,25 @@ import static logic.Direction.*;
 
 public class Main {
 
-    private static int score = 0; //lączna liczba punków
-    private static boolean endGame = false; // wskaznika na koniec gry
+    //wynik dzałanie metody shiftRow(), object zawiera zmieneny wiersz, i bool
+    // wskazujący czy rowny on wartości originalnej
+    private static class ShiftRowResult{
+        boolean moving;
+        int[] shiftedRow;
+    }
+    private static int score; //lączna liczba punków
+    private static boolean endGame; // wskaznika na koniec gry
     private static Field gameField; // pole gry
     private static Direction direction; // wskazniak przycisku
-    private static Graphic graphic = new GraphicConsole();
+    private static Graphic graphic ; //tworzenie nowego interface tekstowego
 
     private static void initFields(){
+        score = 0;
+        endGame = false;
+        graphic = new GraphicConsole();
         gameField = new Field();
     }
+    //tworzy nowe komurki na starcie gry
     private static void createInitialCell(){
         for (int i = 0; i < COUNT_INITAL_CELL ; i++) {
             generateNewCell();
@@ -82,17 +92,112 @@ public class Main {
         }
     }
 
+    private static ShiftRowResult shiftRow(int[] oldRow){
+        ShiftRowResult returnArray = new ShiftRowResult();
+
+        int[] withoutZero = new int[oldRow.length];
+        {
+            int count = 0;
+            for (int i = 0; i < oldRow.length; i++) {
+                if(oldRow[i] != 0){
+                    if(count != i){
+                        returnArray.moving = true;
+                    }
+                    withoutZero[count] = oldRow[i];
+                    count++;
+                }
+            }
+        }
+        returnArray.shiftedRow = new int[withoutZero.length];
+        {
+            int count = 0;
+            {
+                int i = 0;
+                while(i < withoutZero.length){
+                    if(i+1 < withoutZero.length && withoutZero[i] == withoutZero[i+1] && withoutZero[i] != 0){
+                        returnArray.moving = true;
+                        returnArray.shiftedRow[count] = withoutZero[i]*2;
+                        i++;
+                    }else{
+                        returnArray.shiftedRow[count] = withoutZero[i];
+                    }
+                    count++;
+                    i++;
+                }
+            }
+            for (int i = count; i < returnArray.shiftedRow.length ; i++) {
+                returnArray.shiftedRow[i] = 0;
+            }
+        }
+        return returnArray;
+    }
+
     private static boolean shift(Direction direction){
         boolean shift = false;
         switch(direction){
-            case UP: break;
-            case DOWN:break;
-            case LEFT:break;
-            case RIGHT:break;
-            default:break;
+            case UP:
+            case DOWN:
+                for (int i = 0; i < COUNT_CELL_X ; i++) {
+                    int[] col = gameField.getColumn(i);
+
+                    if(direction == UP){
+                        int[] tmp = new int[col.length];
+                        for (int j = 0; j < tmp.length ; j++) {
+                            tmp[j] = col[tmp.length - j-1];
+                        }
+                        col = tmp;
+                    }
+                    ShiftRowResult result = shiftRow(col);
+
+                    if(direction == UP){
+                        int[] tmp = new int[result.shiftedRow.length];
+                        for (int j = 0; j < tmp.length ; j++) {
+                            tmp[j] = result.shiftedRow[tmp.length - j-1];
+                        }
+                        result.shiftedRow = tmp;
+                    }
+                    gameField.setColumn(i, result.shiftedRow);
+                    shift = shift || result.moving;
+                }
+                break;
+
+            case LEFT:
+            case RIGHT:
+                for (int i = 0; i < COUNT_CELL_Y; i++) {
+                    int[] row = gameField.getRow(i);
+
+                    if(direction == RIGHT){
+                        int[] tmp = new int[row.length];
+                        for (int j = 0; j < tmp.length ; j++) {
+                            tmp[j] = row[tmp.length - j-1];
+                        }
+                        row = tmp;
+                    }
+                    ShiftRowResult result = shiftRow(row);
+
+                    if(direction == RIGHT){
+                        int[] tmp = new int[result.shiftedRow.length];
+                        for (int j = 0; j < tmp.length ; j++) {
+                            tmp[j] = result.shiftedRow[tmp.length - j-1];
+                        }
+                        result.shiftedRow = tmp;
+                    }
+
+                    gameField.setRow(i, result.shiftedRow);
+
+                    shift = shift || result.moving;
+                }
+                break;
+
+            default:
+                System.err.println("Wrong parameter on Direction");
+                System.exit(-2);
+                break;
         }
-        return true;
+        return shift ;
     }
+
+
 
     public static void main(String[] args) {
         initFields(); // tworzenie nowego pola
@@ -103,7 +208,6 @@ public class Main {
             //logic();
             graphic.draw(gameField);
         }
-
 
     }
 
--- src/logic/Field.java
+++ src/logic/Field.java
@@ -15,15 +15,43 @@ public class Field {
         }
     }
 
+    //zwraca wartość komurki o podanych wszpołżędnuch
     public int getStateCell(int i, int j) {
         return gameField[i][j];
     }
 
+    // ustawia wartośc komurki state o podanych wspołżędnuch i,j
     public void setStateCell(int i, int j, int state) {
         gameField[i][j] = state;
     }
 
+    //zwraca kolumne i
+    public int[] getColumn(int i){
+        return gameField[i];
+    }
 
+    // przepisuje columne i wartośc nowej kolumny col
+    public void setColumn(int i, int[] col){
+        gameField[i] = col;
+    }
+
+    //zwrace wiersz-tablice pod numerem i
+    public int[] getRow(int i){
+        int[] ret = new int[COUNT_CELL_X];
+        for (int j = 0; j < COUNT_CELL_X; j++) {
+            ret[j] = gameField[j][i];
+        }
+        return ret;
+    }
+
+    // przepisuje wiersz pod numerem i
+    public void setRow(int i, int[] newRow){
+        for (int j = 0; j < COUNT_CELL_X ; j++) {
+            gameField[j][i] = newRow[j];
+        }
+    }
+
+    // zwraca zawartość komurki tablicy z typem string // sprawdzic czy to jest potrebne!!!!
     public String toStringCell(int i, int j) {
         return String.valueOf(gameField[i][j]);
     }
